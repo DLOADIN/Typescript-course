9fd2e0f165eeec1b587731b59ba8aed9b60c14ef
DLOADIN <m.david@alustudent.com>
2026-01-13 14:25:50 +0200

    done 1

---
+++ myarray.ts
@@ -0,0 +1,24 @@
+const firstarray: number[] = [1,2,3,4,5];
+const secondarray: string[] = ['a','b','c','d','e'];
+const Heros: Array<string> = ['Spiderman', 'Ironman', 'Hulk', 'Thor'];
+
+
+type User = {
+    name: string;
+    isActive: boolean;
+}
+
+const activeUsers: User[] = [
+    {
+        name: 'Jean',
+        isActive: true,
+    },
+]
+
+const allusers: User[] = []
+
+firstarray.push(6);
+secondarray.push('f');
+Heros.push('Captain America');
+
+
