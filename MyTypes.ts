1198faacb4d88f2661e84e866517843306f31b2a
DLOADIN <m.david@alustudent.com>
2026-01-13 14:56:53 +0200

    COnstant Evolution

---
+++ MyTypes.ts
@@ -0,0 +1,9 @@
+type Users = [number ,string];
+
+const newUser: Users = [1, 'Jean'];
+
+
+//For the tuples, You can be able to atleast push more values to the tuple but you cannot change the type of the values
+newUser[1]="DOE";
+console.log(newUser);
+//newUser.push(true); //This will give an error because the type is not defined in the tuple
